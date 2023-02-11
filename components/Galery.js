efcadc4256c284c0e578b0674ce1ede83400c68e
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-02-11 20:19:52 +0100

    fix responsive

--- components/Galery.js
+++ components/Galery.js
@@ -2,11 +2,11 @@ import React from "react"
 
 function Galery() {
 	return (
-		<section className="w-full flex  justify-center items-center flex-col md:flex-row">
-			<div className="w-full h-[150px] bg-blue-200 md:w-[25%] md:h-[350px]"></div>
-			<div className="w-full h-[150px] bg-green-300 md:w-[25%] md:h-[350px]"></div>
-			<div className="w-full h-[150px] bg-orange-200 md:w-[25%] md:h-[350px]"></div>
-			<div className="w-full h-[150px] bg-yellow-300 md:w-[25%] md:h-[350px]"></div>
+		<section className="w-full flex  justify-center items-center flex-wrap flex-row md:flex-row">
+			<div className="w-[50%] h-[230px] bg-blue-200 md:w-[25%] md:h-[400px]"></div>
+			<div className="w-[50%] h-[230px] bg-green-300 md:w-[25%] md:h-[400px]"></div>
+			<div className="w-[50%] h-[230px] bg-orange-200 md:w-[25%] md:h-[400px]"></div>
+			<div className="w-[50%] h-[230px] bg-yellow-300 md:w-[25%] md:h-[400px]"></div>
 		</section>
 	)
 }
