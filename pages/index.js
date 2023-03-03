3d1b2561bad1751d0816889254c3181d2622af3c
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-03-03 21:51:57 +0100

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
 					content="Pourvoirie chasse et pêche dans les Hautes Laurentides"
 				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.png" />
 			</Head>
 			<NavBar />
 			<main>
