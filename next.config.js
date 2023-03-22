2b15823492e32c24a01352071ee32dc12a894711
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-03-22 21:47:05 +0100

    add multilingual

--- next.config.js
+++ next.config.js
@@ -6,15 +6,14 @@ const path = require("path")
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-}
-
-module.exports = {
-	nextConfig,
-
 	i18n: {
 		locales: i18n.locales,
 		defaultLocale: i18n.defaultLocale,
 	},
+}
+
+module.exports = {
+	nextConfig,
 
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
